2baa0329e0c93c097d72e0b8d151ab7e55200fbf
DEEPANSHU KUMAR <97106571+DeepanshuK007@users.noreply.github.com>
2024-03-05 21:03:42 +0530

    Screens

---
+++ screens/AppointmentConfirmationScreen.tsx
@@ -0,0 +1,107 @@
+import { Image, StyleSheet, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { darkGreen } from '../sharedStyles';
+import { Text } from 'react-native-paper';
+import CTABigWhite from '../components/CTABigWhite';
+import { AppointmentConfirmationScreenParams } from '../types';
+import * as Notifications from 'expo-notifications';
+import ReactDOMServer from 'react-dom/server';
+import { I18nextProvider, useTranslation } from 'react-i18next';
+import i18n from '../i18n';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+const AppointmentConfirmationScreen = () => {
+  const params = useRoute().params as AppointmentConfirmationScreenParams;
+  const navigation = useNavigation();
+  const { t } = useTranslation();
+  //const upcommingString = ReactDOMServer.renderToString(<I18nextProvider i18n={i18n}> <Text>{t('UpcomingAppointments')}</Text> </I18nextProvider>);
+
+  console.log(params);
+
+  useEffect(() => {
+    (async () => {
+      await Notifications.setBadgeCountAsync(1);
+      const res = await Notifications.scheduleNotificationAsync({
+        content: {
+          title: 'eSanjeevani',
+          subtitle: 'UpcomingAppointments',
+          body: `You have an appointment at ${params.formattedTime}`,
+        },
+        trigger: {
+          seconds: 5,
+        },
+      });
+    })();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.topSection}>
+        <Image
+          source={require('../assets/images/success.png')}
+          resizeMode='contain'
+          style={styles.img}
+        />
+        <Text
+          variant='displayMedium'
+          style={{ color: '#fff', fontWeight: '700', marginBottom: 5 }}
+        >
+          <I18nextProvider i18n={i18n}>{t('Success')}</I18nextProvider>
+        </Text>
+        <Text
+          variant='bodyLarge'
+          style={{ color: '#fff', fontWeight: '700', marginBottom: 40 }}
+        >
+          <I18nextProvider i18n={i18n}>{t('appointmentisset')}</I18nextProvider>
+        </Text>
+        <Text
+          variant='bodyLarge'
+          style={{ color: '#fff', fontWeight: '700', marginBottom: 10 }}
+        >
+          {params.doctor}
+        </Text>
+        <Text
+          variant='bodyLarge'
+          style={{ color: '#fff', fontWeight: '700', marginBottom: 20 }}
+        >
+          {params.formattedTime}
+        </Text>
+        <CTABigWhite
+          icon='calendar'
+          text='See Appointments'
+          onPress={() => navigation.navigate('Root', { screen: 'TabThree' })}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default AppointmentConfirmationScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'transparent',
+  },
+  topSection: {
+    flex: 1,
+    height: '100%',
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: darkGreen,
+    paddingHorizontal: 20,
+  },
+  img: {
+    width: 250,
+    height: 250,
+  },
+});
---
+++ screens/AppointmentScreen.tsx
@@ -0,0 +1,204 @@
+import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+import OneHealSafeArea from "../components/OneHealSafeArea";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Doctor as DoctorType } from "../types";
+import Doctor from "../components/Doctor";
+import DateItem from "../components/DateItem";
+import CTABig from "../components/CTABig";
+import {
+  ActivityIndicator,
+  Button,
+  Dialog,
+  Portal,
+  Text,
+} from "react-native-paper";
+import { useAppointments } from "../hooks/useAppointments";
+import { darkGreen } from "../sharedStyles";
+import { I18nextProvider, useTranslation } from "react-i18next";
+import i18n from "../i18n";
+import {
+  AddAppointmentParams,
+  useAddAppointment,
+} from "../hooks/useAddAppointment";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+const AppointmentScreen = () => {
+  const addAppointmentMutation = useMutation({
+    mutationKey: ["addAppointment"],
+    mutationFn: async (params: AddAppointmentParams) =>
+      axios.post(
+        "https://lachs.informatik.tu-chemnitz.de/planspiel/v1/addappointment",
+        params
+      ),
+  });
+
+  const navigation = useNavigation();
+  const params = useRoute().params as DoctorType;
+  const [time, setTime] = useState("");
+  const [choosenDay, setChoosenDay] = useState("");
+  const [choosenDate, setChoosenDate] = useState("");
+  const [visible, setVisible] = React.useState(false);
+  const { data: dataAppointments, isLoading: isLoadingAppointments } =
+    useAppointments({ doctorId: params.doctorId, enabled: true });
+  const { t } = useTranslation();
+  const [chosenTimeSlotId, setChosenTimeSlotId] = useState(0);
+
+  const showDialog = () => setVisible(true);
+  const hideDialog = () => setVisible(false);
+
+  const confirmAppointment = ({
+    timeSlotId,
+    formattedTime,
+  }: {
+    timeSlotId: number;
+    formattedTime: string;
+  }) => {
+    setVisible(false);
+    addAppointmentMutation.mutate({
+      appointmentId: chosenTimeSlotId,
+      isPreliminaryCheckup: true,
+      patientId: 1,
+    });
+    navigation.navigate("AppointmentStack", {
+      screen: "AppointmentConfirmationScreen",
+      params: { formattedTime, timeSlotId, doctor: params.name },
+    });
+  };
+
+  // console.log(
+  //   'apps are ',
+  //   moment('2023-03-05T08:00:00.000Z').format('DD-MM-YYYY HH:mm'),
+  //   FULL_WEEK[date.getDay()]
+  //   // date.getFullYear() + '-' + date.getMonth()+1 + '-'
+  // );
+
+  const map = dataAppointments?.reduce((acc, curr) => {
+    const { startDate, timeSlotId } = curr;
+    const [date, time] = startDate.split("T");
+    const [hh, mm, ss] = time.split(":");
+    const formattedTime = `${hh}:${mm}`;
+    if (!acc.has(date)) {
+      acc.set(date, {
+        startDate: date,
+        time: [...acc, { timeSlotId: timeSlotId, formattedTime }],
+      });
+    } else acc.get(date).time.push({ timeSlotId: timeSlotId, formattedTime });
+    return acc;
+  }, new Map());
+  let result;
+  if (map) result = [...map.values()];
+
+  return (
+    <OneHealSafeArea statusBar="light">
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={{
+            marginTop: 20,
+            borderRadius: 50,
+            borderColor: darkGreen,
+            borderWidth: 2,
+            width: 34,
+            height: 34,
+            justifyContent: "center",
+            alignItems: "center",
+            marginLeft: 20,
+          }}
+        >
+          <MaterialCommunityIcons name="arrow-left" size={24} />
+        </TouchableOpacity>
+        <Doctor
+          specialization={params.specialization}
+          name={params.name}
+          key={params.doctorId}
+          doctorId={params.doctorId}
+          locationId={params.locationId}
+          styles={{ marginTop: 20, backgroundColor: "#fff" }}
+          chosenLocation={params.chosenLocation}
+          chosenLocationName={params.chosenLocationName}
+        />
+
+        <ScrollView style={{ flex: 1 }}>
+          {!dataAppointments || isLoadingAppointments ? (
+            <ActivityIndicator animating color={darkGreen} size="large" />
+          ) : isLoadingAppointments ? (
+            <ActivityIndicator animating color={darkGreen} size="large" />
+          ) : result ? (
+            result.map((data, index: React.Key | null | undefined) => (
+              <DateItem
+                key={index}
+                day={data.startDate}
+                date={data.startDate}
+                time={data.time}
+                setTime={setTime}
+                doctor={`${params.name}`}
+                choosenDate={choosenDate}
+                setChoosenDate={setChoosenDate}
+                choosenDay={choosenDay}
+                setChoosenDay={setChoosenDay}
+                setVisible={setVisible}
+                visible={visible}
+                chosenTimeSlotId={chosenTimeSlotId}
+                setChosenTimeSlotId={setChosenTimeSlotId}
+              />
+            ))
+          ) : null}
+
+          <Portal>
+            <Dialog visible={visible} onDismiss={hideDialog}>
+              <Dialog.Title>
+                <I18nextProvider i18n={i18n}>
+                  {" "}
+                  <Text>{t("ConfirmAppointment")}</Text>{" "}
+                </I18nextProvider>
+              </Dialog.Title>
+              <Dialog.Content>
+                <Text variant="bodyLarge" style={{ fontWeight: "500" }}>
+                  {params.name}
+                </Text>
+              </Dialog.Content>
+              <Dialog.Content>
+                <Text variant="bodyMedium">{choosenDay}</Text>
+                <Text variant="bodyMedium">{choosenDate}</Text>
+              </Dialog.Content>
+              <Dialog.Actions>
+                <Button
+                  mode="outlined"
+                  textColor={darkGreen}
+                  onPress={() =>
+                    confirmAppointment({
+                      formattedTime: choosenDay,
+                      timeSlotId: chosenTimeSlotId,
+                    })
+                  }
+                >
+                  <I18nextProvider i18n={i18n}>
+                    {" "}
+                    <Text>{t("Confirm")}</Text>{" "}
+                  </I18nextProvider>
+                </Button>
+                <Button onPress={hideDialog}>
+                  <I18nextProvider i18n={i18n}>
+                    {" "}
+                    <Text>{t("Cancel")}</Text>{" "}
+                  </I18nextProvider>
+                </Button>
+              </Dialog.Actions>
+            </Dialog>
+          </Portal>
+        </ScrollView>
+      </View>
+    </OneHealSafeArea>
+  );
+};
+
+export default AppointmentScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
